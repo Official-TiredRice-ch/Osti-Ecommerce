53a4848ca2e3e12fc0abf38b75ff7be580ef9754
tiredrice <lou.ch.clippers@gmail.com>
2026-03-16 10:59:44 +0800

    New Changes

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const logger = require('./utils/logger');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 24365;
 
 // Middleware
 app.use(cors({
